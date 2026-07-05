58ee1dcce7cfdef7dfa55c536f919fab1a8e1e86
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-07-05 22:21:06 +0700

    use names instead ad_id

--- make_id_file.cjs
+++ make_id_file.cjs
@@ -5,10 +5,22 @@ const escape_html = html =>
         .replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
+        
+const v2e = _ => _
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/đ/g, "d")
+    .replace(/Đ/g, "D")
+
+const names = new Set()
 
 module.exports = (html, ad) => {
     const subject = escape_html(ad.subject)
     const body = escape_html(ad.body)
+    var name = v2e(ad.subject.toLowerCase()).replace(/[^a-z0-9]+/g, '-').replace(/^-|-$/g, '')
+    
+    if (names.has(name)) name += ('-' + ad.price / 1e6).replace('.', 'm')
+    if (names.has(name)) name += '-' + ad.ad_id
     
     html = html.replace(
         '<div id="map"></div>',
@@ -16,7 +28,7 @@ module.exports = (html, ad) => {
     )
     html = html.replace(
         '<title>Danang Rent Map</title>',
-        `<title>${subject}</title>`
+        `<title>${subject}</title><meta name="ad:id" content="${name}">`
     )
     html = html.replace(
         '<meta property="og:title" content="Danang Rent Map" />',
@@ -24,7 +36,7 @@ module.exports = (html, ad) => {
     )
     html = html.replace(
         '<meta property="og:url" content="https://danang.kim/">',
-        `<meta property="og:url" content="https://danang.kim/id/${ad.ad_id}">`
+        `<meta property="og:url" content="https://danang.kim/id/${name}">`
     )
     html = html.replace(
         '<meta name="description" content="Interactive map for Danang, Vietnam with rental ads" />',
@@ -36,7 +48,7 @@ module.exports = (html, ad) => {
     )
     html = html.replace(
         '<link rel="canonical" href="https://danang.kim/">',
-        `<link rel="canonical" href="https://danang.kim/id/${ad.ad_id}">`,
+        `<link rel="canonical" href="https://danang.kim/id/${name}">`,
     )
     html = html.replace(
         '"@type":"WebSite"',
@@ -48,8 +60,10 @@ module.exports = (html, ad) => {
     )
     html = html.replace(
         '"url":"https://danang.kim/"',
-        `"url":"https://danang.kim/id/${ad.ad_id}",\n\t\t\t"price":"${ad.price}",\n\t\t\t"priceCurrency":"VND"`
+        `"url":"https://danang.kim/id/${name}",\n\t\t\t"price":"${ad.price}",\n\t\t\t"priceCurrency":"VND"`
     )
     
-    fs.writeFileSync('id/' + ad.ad_id + '.html', html)
+    fs.writeFileSync('id/' + name + '.html', html)
+    
+    return name
 }
