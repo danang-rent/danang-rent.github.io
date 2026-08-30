400eb322d813648fdbcf2d5ec606ec96d8c70145
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-08-30 14:33:37 +0700

    cache manifest.json for offline mode

--- sw.js
+++ sw.js
@@ -2,6 +2,21 @@ self.addEventListener('fetch', event => {
     if (event.request.mode !== 'navigate')
         return
 
+    if (event.request.url.endsWith('manifest.json')) {
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    const clone = response.clone()
+                    caches.open('manifest.json').then(cache =>
+                        cache.put('manifest.json', clone)
+                    )
+                    return response
+                })
+                .catch(() => caches.match('manifest.json'))
+        )
+        return
+    }
+
     if (new URL(event.request.url).pathname !== '/')
         return
 
