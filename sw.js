59d51926203c8983675ed01512944c3d3401d7f8
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-08-30 14:59:54 +0700

    cache imag-*.png

--- sw.js
+++ sw.js
@@ -29,6 +29,36 @@ self.addEventListener('fetch', event => {
         return
     }
 
+    if (event.request.url.endsWith('icon-192.png')) {
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    const clone = response.clone()
+                    caches.open('icon-192.png').then(cache =>
+                        cache.put('icon-192.png', clone)
+                    )
+                    return response
+                })
+                .catch(() => caches.match('icon-192.png'))
+        )
+        return
+    }
+
+    if (event.request.url.endsWith('icon-512.png')) {
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    const clone = response.clone()
+                    caches.open('icon-512.png').then(cache =>
+                        cache.put('icon-512.png', clone)
+                    )
+                    return response
+                })
+                .catch(() => caches.match('icon-512.png'))
+        )
+        return
+    }
+
     if (event.request.mode !== 'navigate')
         return
 
@@ -55,6 +85,10 @@ self.addEventListener('install', event => {
                 .then(cache => cache.add('/')),
             caches.open('manifest.json')
                 .then(cache => cache.add('manifest.json')),
+            caches.open('icon-192.png')
+                .then(cache => cache.add('icon-192.png')),
+            caches.open('icon-512.png')
+                .then(cache => cache.add('icon-512.png')),
             caches.open('favicon.ico')
                 .then(cache => cache.add('favicon.ico'))
         ])
