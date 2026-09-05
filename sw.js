e44127a227ba3dc46eaa6fe0e749dc83ee7eee3e
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-09-05 14:03:56 +0700

    close #67 - retry + default and fix wrong position from ad

--- sw.js
+++ sw.js
@@ -2,19 +2,44 @@
 var photos_cache
 caches.open('photos').then(_ => photos_cache = _)
 
+const request_photo = ({url, retry}) =>
+    fetch(url, {mode: 'no-cors'})
+    .then(r =>
+        (r.ok || r.type == 'opaque') &&
+        photos_cache.put(url, r.clone())
+        .then(
+            () => r,
+            e => {
+                console.error('sw.js', '1. CacheStorage put', 'url:', url, 'error:', e, 'respond:', r, 'retry:', retry)
+                debugger
+                //here need retrun `r` anyway
+                return r
+            }
+        )
+    )
+    .catch(e => {
+        console.error('sw.js', '2. fetch(mode no-cors)', 'url:', url, 'error:', e)
+        debugger
+        if (retry > 0) {
+            return request_photo({url, retry: retry - 1})
+        }
+    })
+
 self.addEventListener('fetch', event => {
     const u = event.request.url
     if (u.includes('cdn.chotot.com') && u.endsWith('.jpg')) {
         event.respondWith(
             photos_cache.match(u)
             .then(cached_r =>
-                cached_r || fetch(u, {mode: 'no-cors'})
-                .then(r =>
-                    (r.ok || r.type == 'opaque') &&
-                    photos_cache.put(u, r.clone())
-                    .then(() => r)
-                )
+                cached_r || request_photo({url: u, retry: 2})
             )
+            .catch(e => {
+                console.error('sw.js', '3. JPG last catch - load default', e)
+                return photos_cache.match('/photo-default.jpg') 
+                .then(cached_r =>
+                    cached_r || request_photo({url: '/photo-default.jpg', retry: 0})
+                )
+            })
         )
         return
     }
